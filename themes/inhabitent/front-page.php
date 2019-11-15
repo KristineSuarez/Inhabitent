03a51c30b81ac67873353b8f001477366020b00a
Kristine Suarez <suarezkristine_001@live.ca>
2019-11-14 20:18:34 -0800

    Add widget plugin and updated front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -19,29 +19,48 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+	<section class="product-info container">
+            <h2>Shop Stuff</h2>
+            <?php
+               $terms = get_terms( array(
+                   'taxonomy' => 'product_type',
+                   'hide_empty' => 0,
+               ) );
+               if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+            ?>
+               <div class="product-type-blocks">
+                  <?php foreach ( $terms as $term ) : ?>
+                     <div class="product-type-block-wrapper">
+						<img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+
+						
+                        <p><?php echo $term->description; ?></p>
+                        <p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+                     </div>
+                  <?php endforeach; ?>
+               </div>
+            <?php endif; ?>
+         </section>
+
 	<?php
    		$args = array( 
-			   'post_type' => 'product', 
-			   'post_per_page' => 3,
+			   'post_type' => 'post', 
+			   'posts_per_page' => 3,
 			   'order' => 'ASC');
-			//    'tax_query' => array(
-			// 	array(
-			// 	   'taxonomy' => 'product-posts',
-			// 	   'field'    => 'slug',
-				//    'terms'    => 'bread',
    		$product_posts = get_posts( $args ); // returns an array of posts
 	?>
+
+	<section class="fp-journals">
 	<?php 
 		foreach ( $product_posts as $post ) : setup_postdata( $post ); 
-		the_title();
-		the_content();
-	?>
-	
-	<?php 
-
-   ?>
-<?php endforeach; wp_reset_postdata(); ?>
-
+		echo "<div class='fp-journal'>";
+			the_post_thumbnail('large');
+			the_title();
+			comments_number();
+		echo "</div>";
+		
+		endforeach; wp_reset_postdata();  ?>
+	</section>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
