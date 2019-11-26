6fac315f2d8e298fa6308f8a820f7f4abac67210
Kristine Suarez <suarezkristine_001@live.ca>
2019-11-25 21:43:55 -0800

    Updated Adventures on front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -106,8 +106,8 @@ get_header(); ?>
 					class="adventure-post-pic" src="<?php echo get_template_directory_uri() . '/images/adventure-photos/canoe-girl.jpg'?>" alt="girl in a canoe"
 				>
 				<div class="adv-title">
-						<h3 class="adv-title-canoe title-grid"><a href="">Getting Back to Nature in a Canoe</a></h3>
-						<p class="adv-read">READ MORE</p>
+						<h3 class="at-canoe title-grid"><a href="">Getting Back to Nature in a Canoe</a></h3>
+						<p class="read-canoe">READ MORE</p>
 				</div>
 			</div>
 			
@@ -116,8 +116,8 @@ get_header(); ?>
 					class="adventure-post-pic" src="<?php echo get_template_directory_uri() . '/images/adventure-photos/beach-bonfire.jpg'?>" alt="bonfire"
 				>
 				<div class="adv-title">
-					<h3 class="adv-title-beach title-grid"><a href="">A Night with Friends at the Beach</a></h3>	
-					<p class="adv-read">READ MORE</p>				
+					<h3 class="at-beach title-grid"><a href="">A Night with Friends at the Beach</a></h3>	
+					<p class="read-beach">READ MORE</p>				
 				</div>
 			</div>
 			
@@ -126,8 +126,8 @@ get_header(); ?>
 					class="adventure-post-pic" src="<?php echo get_template_directory_uri() . '/images/adventure-photos/mountain-hikers.jpg'?>" alt="hikers"
 				>
 				<div class="adv-title">
-					<h3 class="adv-title-mountain title-grid"><a href="">Taking in the View at the Big Mountain</a></h3>
-					<p class="adv-read">READ MORE</p>
+					<h3 class="at-mountain title-grid"><a href="">Taking in the View at the Big Mountain</a></h3>
+					<p class="read-mountain">READ MORE</p>
 
 				</div>
 			</div>
@@ -136,65 +136,13 @@ get_header(); ?>
 					class="adventure-post-pic" src="<?php echo get_template_directory_uri() . '/images/adventure-photos/night-sky.jpg'?>" alt="Star-gazing"
 				>
 				<div class="adv-title">
-					<h3 class="adv-title-star title-grid"><a href="">Star-Gazing at the Night Sky</a></h3>
-					<p class="adv-read">READ MORE</p>
+					<h3 class="at-star title-grid"><a href="">Star-Gazing at the Night Sky</a></h3>
+					<p class="read-star">READ MORE</p>
 				</div>
-
-
 			</div>
+
+			<p class="more-adventures">MORE ADVERNTURES</p>
 		</div>
-		<!-- <ul class="adventure-entries">
-			<li class="adventure-post">
-				<div class="adv-img">
-					<img 
-						class="adventure-post-pic" src="<php echo get_template_directory_uri() . '/images/adventure-photos/canoe-girl.jpg'?>" alt="girl in a canoe">
-				</div>
-
-					<div class="adv-title">
-						<h3 class="adv-title-grid"><a href="">Getting Back to Nature in a Canoe</a></h3>
-						<p class="adv-read">READ MORE</p>
-					</div>
-			</li>
-				
-
-			<li class="adventure-post">
-				<div class="adv-img">
-
-					<img 
-						class="adventure-post-pic" src="<php echo get_template_directory_uri() . '/images/adventure-photos/beach-bonfire.jpg'?>" alt="bonfire">
-				</div>
-
-				<div class="adv-title">
-					<h3 class="adv-title-grid"><a href="">A Night with Friends at the Beach</a></h3>	
-					<p class="adv-read">READ MORE</p>				
-				</div>
-			</li>
-
-			<li class="adventure-post">
-				<div class="adv-img">
-					<img 
-						class="adventure-post-pic" src="<php echo get_template_directory_uri() . '/images/adventure-photos/mountain-hikers.jpg'?>" alt="hikers">
-				</div>
-
-				<div class="adv-title">
-					<h3 class="adv-title-grid"><a href="">Taking in the View at the Big Mountain</a></h3>
-					<p class="adv-read">READ MORE</p>
-
-				</div>
-			</li>
-
-			<li class="adventure-post">
-				<div class="adv-img">
-					<img 
-						class="adventure-post-pic" src="<php echo get_template_directory_uri() . '/images/adventure-photos/night-sky.jpg'?>" alt="Star-gazing">
-				</div>
-
-				<div class="adv-title">
-					<h3 class="adv-title-grid"><a href="">Star-Gazing at the Night Sky</a></h3>
-					<p class="adv-read">READ MORE</p>
-				</div>
-			</li>
-		</ul> -->
 
 	</section>
 
--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -12,8 +12,25 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<!-- <php the_title( '<h1 class="entry-title">', '</h1>' ); ?> -->
+				</header><!-- .entry-header -->
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+					<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
+
+
+
+				<!-- <php get_template_part( 'template-parts/content', 'page' ); ?> -->
 
 			<?php endwhile; // End of the loop. ?>
 
